8b1f4a4fb06bced356961d18bdb09250faf1d638
yuki3709 <32707335+yuki3709@users.noreply.github.com>
2018-06-11 20:42:36 +0900

    複数人の命令を反映

--- public/js_screen/index.js
+++ public/js_screen/index.js
@@ -1,4 +1,4 @@
-function Circle(speedX, speedY, locX, locY) {
+function Circles(speedX, speedY, locX, locY) {
     this.speedX = speedX;
     this.speedY = speedY;
     this.locX = locX;
@@ -9,41 +9,41 @@ var circle = [];
 var circle0 = [];
 var order = [];
 var pnum = 0;
+order[pnum] = [];
 
-circle[0] = new Circle(3.0, 4.0, 200, 150);
-circle[1] = new Circle(-4.0, -3.0, 100, 50);
-circle0[0] = new Circle(3.0, 4.0, 200, 150);
+circle[0] = new Circles(3.0, 4.0, 200, 150);
+circle[1] = new Circles(-4.0, -3.0, 100, 50);
+circle0[0] = new Circles(3.0, 4.0, 200, 150);
 
 
 function draw() {
     context.globalCompositeOperation = "source-over";
     context.fillStyle = "rgb(8,8,12)";
     context.fillRect(0, 0, 400, 300);
-    if (order[ordernum].roll) {
-
-        if (circle[0].speedX == 0 && circle[0].speedY > 0) {
-            circle[0].speedX = circle0[0].speedX;
+    if (order[pnum][ordernum].roll) {
+        if (circle[pnum].speedX == 0 && circle[pnum].speedY > 0) {
+            circle[pnum].speedX = circle0[0].speedX;
         }
-        else if (circle[0].speedX > 0 && circle[0].speedY > 0) {
-            circle[0].speedY = 0;
+        else if (circle[pnum].speedX > 0 && circle[pnum].speedY > 0) {
+            circle[pnum].speedY = 0;
         }
-        else if (circle[0].speedX > 0 && circle[0].speedY == 0) {
-            circle[0].speedY = circle0[0].speedY * -1;
+        else if (circle[pnum].speedX > 0 && circle[pnum].speedY == 0) {
+            circle[pnum].speedY = circle0[0].speedY * -1;
         }
-        else if (circle[0].speedX > 0 && circle[0].speedY < 0) {
-            circle[0].speedX = 0;
+        else if (circle[pnum].speedX > 0 && circle[pnum].speedY < 0) {
+            circle[pnum].speedX = 0;
         }
-        else if (circle[0].speedX == 0 && circle[0].speedY < 0) {
-            circle[0].speedX = circle0[0].speedX * -1;
+        else if (circle[pnum].speedX == 0 && circle[pnum].speedY < 0) {
+            circle[pnum].speedX = circle0[0].speedX * -1;
         }
-        else if (circle[0].speedX < 0 && circle[0].speedY < 0) {
-            circle[0].speedY = 0;
+        else if (circle[pnum].speedX < 0 && circle[pnum].speedY < 0) {
+            circle[pnum].speedY = 0;
         }
-        else if (circle[0].speedX < 0 && circle[0].speedY == 0) {
-            circle[0].speedY = circle0[0].speedY;
+        else if (circle[pnum].speedX < 0 && circle[pnum].speedY == 0) {
+            circle[pnum].speedY = circle0[0].speedY;
         }
-        else if (circle[0].speedX < 0 && circle[0].speedY > 0) {
-            circle[0].speedX = 0;
+        else if (circle[pnum].speedX < 0 && circle[pnum].speedY > 0) {
+            circle[pnum].speedX = 0;
         }
     }
     //位置を更新
@@ -83,8 +83,8 @@ function init() {
         var e = document.createElement('p');
         e.innerText = data.id;
         message.appendChild(e);
-        order = data.command;
-
+        order[pnum] = data[pnum].command;
+        pnum++;
     });
     if (canvas.getContext) {
         context = canvas.getContext('2d');
