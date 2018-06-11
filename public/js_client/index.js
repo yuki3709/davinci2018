aaba40aeebbad92c77f6cc765293a135c251bec8
Sahara62 <cloud_1827_862@yahoo.co.jp>
2018-06-11 22:22:52 +0900

    goボタンの定義
    goボタンが押されたとき，commandに{"go":10}を追加

--- public/js_client/index.js
+++ public/js_client/index.js
@@ -7,7 +7,12 @@ var prop = {
 		],
 	"id":"中村"
 };
+
 window.onload = function () {
+  var go = document.getElementById('go');
+  go.addEventListener('click',function(){
+    prop.command.push('{"go":"10"}');
+  });
   var send = document.getElementById('send');
   send.addEventListener('click',function(d){
     socket.emit('message', JSON.stringify(prop));
