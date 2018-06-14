80267859a80c6ba7435849ee2b875dda62e99622
Sahara62 <cloud_1827_862@yahoo.co.jp>
2018-06-14 18:19:38 +0900

    goとrollを押したときにwindow上に表示させる

--- public/js_client/index.js
+++ public/js_client/index.js
@@ -7,11 +7,13 @@ window.onload = function () {
   var go = document.getElementById('go');
   go.addEventListener('click',function(){
     prop.command.push({go:10});
+    document.getElementById('messageList').textContent = JSON.stringify(prop);
     console.log(prop);
   });
   var roll = document.getElementById('roll');
   roll.addEventListener('click',function(){
     prop.command.push({roll:10});
+    document.getElementById('messageList').textContent = JSON.stringify(prop);
     console.log(prop);
   });
   var name = document.getElementById('message');
@@ -30,6 +32,7 @@ window.onload = function () {
   var send = document.getElementById('send');
   send.addEventListener('click',function(){
     console.log(prop);
+    if(prop.id === 0) return fales;
     socket.emit('message', JSON.stringify(prop));
   });
 };
