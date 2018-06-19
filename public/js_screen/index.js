5d13d178a05401bf4b8e79e46252d75276872f1b
yuki3709 <32707335+yuki3709@users.noreply.github.com>
2018-06-19 16:33:18 +0900

    プロトタイプを使った
    Circle.prototype.drawとCircle.prototype.discriminateCommandを作成

--- public/js_screen/index.js
+++ public/js_screen/index.js
@@ -1,99 +1,92 @@
 var defaultProps = {
-    speedX: 5.0,
-    speedY: 5.0,
-    locX: 200,
-    locY: 150
-}
-function Circles(props) {
-    this.commandCount = 0;
-    this.command = props.command;
-    this.id = props.id;
-    this.speedX = defaultProps.speedX;
-    this.speedY = defaultProps.speedY;
-    this.locX = defaultProps.locX;
-    this.locY = defaultProps.locY;
-}
-var circle = [];
-
+  speedX: 5.0,
+  speedY: 5.0,
+  locX: 200,
+  locY: 150
+};
+let context;
+const Circle = function (props) {
+  this.commandCount = 0;
+  this.command = props.command;
+  this.id = props.id;
+  this.speedX = defaultProps.speedX;
+  this.speedY = defaultProps.speedY;
+  this.locX = defaultProps.locX;
+  this.locY = defaultProps.locY;
+};
+Circle.prototype.draw = function (context) {
+  context.beginPath();
+  context.fillStyle = '#3399FF';
+  context.arc(this.locX, this.locY, 10, 0, Math.PI * 2.0, true);
+  context.fill();
+  context.fillStyle = 'white';
+  context.fillText(this.id, this.locX - 5, this.locY)
+};
+Circle.prototype.discriminateCommand = function () {
+  var order = this.command[this.commandCount];
+  this.commandCount = (this.commandCount + 1) % this.command.length;
+  var { speedX, speedY } = this;
+  if (order.roll) {
+    if (speedX == 0) {
+      if (speedY < 0) {
+        this.speedX = defaultProps.speedX;
+      } else if (speedY > 0) {
+        this.speedX = defaultProps.speedX * -1;
+      }
+    } else if (speedX > 0) {
+      if (speedY > 0) {
+        this.speedX = 0;
+      } else if (speedY == 0) {
+        this.speedY = defaultProps.speedY;
+      } else {
+        this.speedY = 0;
+      }
+    } else {
+      if (speedY < 0) {
+        this.speedX = 0;
+      } else if (speedY == 0) {
+        this.speedY = defaultProps.speedY * -1;
+      } else {
+        this.speedY = 0;
+      }
+    }
+  }
+  if (order.go) {
+    var futureLocX = this.locX + this.speedX;
+    var futureLocY = this.locY + this.speedY;
+    if (futureLocX - 10 < 0 || futureLocX + 10 > 2000) {
+      this.speedX *= -1;
+    }
+    if (futureLocY - 10 < 0 || futureLocY + 10 > 900) {
+      this.speedY *= -1;
+    }
+    this.locX += this.speedX;
+    this.locY += this.speedY;
+  }
+};
+let circles = [];
 function draw() {
-    context.globalCompositeOperation = "source-over";
-    context.fillStyle = "rgb(8,8,12)";
-    context.fillRect(0, 0, 2000, 900);
-    discriminateCommand();
-    circle.forEach(function (circle) {
-        context.beginPath();
-        context.fillStyle = '#3399FF';
-        context.arc(circle.locX, circle.locY, 10, 0, Math.PI * 2.0, true);
-        context.fill();
-        context.fillStyle = 'white';
-        context.fillText(circle.id, circle.locX - 5, circle.locY)
-    });
+  context.globalCompositeOperation = "source-over";
+  context.fillStyle = "rgb(8,8,12)";
+  context.fillRect(0, 0, 2000, 900);
+  discriminateCommand();
+  circles.forEach(function (circle) {
+    circle.draw(context);
+  });
 }
 function discriminateCommand() {
-    circle.forEach(function (circle) {
-        var order = circle.command[circle.commandCount];
-        circle.commandCount = (circle.commandCount + 1) % circle.command.length;
-        var { speedX, speedY } = circle;
-        if (order.roll) {
-            if (speedX == 0) {
-                if (speedY < 0) {
-                    circle.speedX = defaultProps.speedX;
-                }
-                else if (speedY > 0) {
-                    circle.speedX = defaultProps.speedX * -1;
-                }
-            }
-            else if (speedX > 0) {
-                if (speedY > 0) {
-                    circle.speedX = 0;
-                }
-                else if (speedY == 0) {
-                    circle.speedY = defaultProps.speedY;
-                }
-                else {
-                    circle.speedY = 0;
-                }
-            }
-            else {
-                if (speedY < 0) {
-                    circle.speedX = 0;
-                }
-                else if (speedY == 0) {
-                    circle.speedY = defaultProps.speedY * -1;
-                }
-                else {
-                    circle.speedY = 0;
-                }
-            }
-        }
-        if (order.go) {
-            var futurelocX = circle.locX + circle.speedX;
-            var futurelocY = circle.locY + circle.speedY;
-            if (futurelocX - 10 < 0 || futurelocX + 10 > 2000) {
-                circle.speedX *= -1;
-            }
-            if (futurelocY - 10 < 0 || futurelocY + 10 > 900) {
-                circle.speedY *= -1;
-            }
-            circle.locX += circle.speedX;
-            circle.locY += circle.speedY;
-        }
-
-    });
+  circles.forEach(function (circle) {
+    circle.discriminateCommand();
+  });
 }
-function init() {
-    var canvas = document.getElementById('tutorial');
-    socket.on('receiveMessage', function (d) {
-        var data = JSON.parse(d); // 文字列→JSON
-        circle.push(new Circles(data));
-        console.log(data);
-    });
-    if (canvas.getContext) {
-        context = canvas.getContext('2d');
-        setInterval(draw, 33);
-    }
-}
-
 window.onload = function () {
-    init();
+  var canvas = document.getElementById('tutorial');
+  socket.on('receiveMessage', function (d) {
+    var data = JSON.parse(d); // 文字列→JSON
+    circles.push(new Circle(data));
+  });
+  if (canvas.getContext) {
+    context = canvas.getContext('2d');
+    setInterval(draw, 33);
+  }
 };
