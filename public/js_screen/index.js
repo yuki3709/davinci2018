91890e199368b07d699268ef0580e7614bce52e4
yuki3709 <32707335+yuki3709@users.noreply.github.com>
2018-06-25 21:24:17 +0900

    記法を変えた
    ラムダ式に記法を変更
    Circle.prototype.discriminateCommand = function (){};から
    Circle.prototype = {
    discriminateCommand: () =>{}};
    に変更

--- public/js_screen/index.js
+++ public/js_screen/index.js
@@ -4,11 +4,17 @@ const defaultProps = {
   direction: 45
 };
 const Bound = {};
-Bound.Field = function (e) {
-  this.init(e);
+Bound.Field = e => {
+  const self = this;
+  this.canvas = e;
+  if (!this.canvas.getContext) throw new Error("contextが見つかりません");
+  this.context = this.canvas.getContext('2d');
+  this.context.globalCompositeOperation = "source-over";
+  setInterval(self.run, 33);
 };
 let context;
-const Circle = function (props) {
+const Circle = data => {
+  const props = JSON.parse(data);
   this.commandCount = 0;
   this.command = props.command;
   this.id = props.id;
@@ -16,78 +22,82 @@ const Circle = function (props) {
   this.locY = defaultProps.locY;
   this.direction = defaultProps.direction;
 };
-
-Circle.prototype.draw = function (context) {
-  context.beginPath();
-  context.fillStyle = '#3399FF';
-  context.arc(this.locX, this.locY, 10, 0, Math.PI * 2.0, true);
-  context.fill();
-  context.fillStyle = 'white';
-  context.fillText(this.id, this.locX - 5, this.locY)
-};
-Circle.prototype.roll = function (direction) {
-  this.direction = this.normalizeDirection(direction + this.direction);
-};
-Circle.prototype.go = function (distance) {
-  const radian = this.direction * Math.PI / 180;
-  let distanceX = distance * Math.cos(radian);
-  let distanceY = distance * Math.sin(radian);
-  var futureLocX = this.locX + distanceX;
-  var futureLocY = this.locY + distanceY;
-  let direction = this.direction;
-  if (futureLocX - 10 < 0 || futureLocX + 10 > 2000) {
-    distanceX *= -1;
-    direction = 180 - direction;
-  }
-  if (futureLocY - 10 < 0 || futureLocY + 10 > 900) {
-    distanceY *= -1;
-    direction *= -1;
-  }
-  this.direction = this.normalizeDirection(direction);
-  this.locX += distanceX;
-  this.locY += distanceY;
-};
-Circle.prototype.normalizeDirection = function (direction) {
-  return (direction + 360) % 360;
-};
-Circle.prototype.discriminateCommand = function () {
-  var order = this.command[this.commandCount];
-  this.commandCount = (this.commandCount + 1) % this.command.length;
-  var { speedX, speedY } = this;
-  if (order.roll) {
-    this.roll(45);
-  }
-  if (order.go) {
-    this.go(5);
+Bound.Field.prototype = {
+  canvas: null,
+  context: null,
+  size: {
+    width: 0,
+    height: 0
+  },
+  components: [],
+  constructor: Bound.Field,
+  clear: () => {
+    let context = this.context;
+    if (!context) return;
+    let size = this.size || {};
+    context.fillStyle = "#000";
+    context.fillRect(0, 0, size.width, size.height);
+  },
+  resize: parent => {
+    this.size.width = this.canvas.width = parent.clientWidth;
+    this.size.height = this.canvas.height = parent.clientHeight;
+  },
+  draw: () => {
+    context.globalCompositeOperation = "source-over";
+    context.fillStyle = "rgb(8,8,12)";
+    context.fillRect(0, 0, 2000, 900);
+  },
+  run: () => {
+    this.draw();
+    discriminateCommand();
+    circles.forEach(circle => circle.draw(context));
   }
 };
-
+Circle.prototype = {
+  draw: context => {
+    context.beginPath();
+    context.fillStyle = '#3399FF';
+    context.arc(this.locX, this.locY, 10, 0, Math.PI * 2.0, true);
+    context.fill();
+    context.fillStyle = 'white';
+    context.fillText(this.id, this.locX - 5, this.locY)
+  },
+  roll: direction => this.direction = this.normalizeDirection(direction + this.direction),
+  go: distance => {
+    const radian = this.direction * Math.PI / 180;
+    let distanceX = distance * Math.cos(radian);
+    let distanceY = distance * Math.sin(radian);
+    var futureLocX = this.locX + distanceX;
+    var futureLocY = this.locY + distanceY;
+    let direction = this.direction;
+    if (futureLocX - 10 < 0 || futureLocX + 10 > 2000) {
+      distanceX *= -1;
+      direction = 180 - direction;
+    }
+    if (futureLocY - 10 < 0 || futureLocY + 10 > 900) {
+      distanceY *= -1;
+      direction *= -1;
+    }
+    this.direction = this.normalizeDirection(direction);
+    this.locX += distanceX;
+    this.locY += distanceY;
+  },
+  normalizeDirection: direction => (direction + 360) % 360,
+  discriminateCommand: () => {
+    var order = this.command[this.commandCount];
+    this.commandCount = (this.commandCount + 1) % this.command.length;
+    var { speedX, speedY } = this;
+    if (order.roll) {
+      this.roll(45);
+    }
+    if (order.go) {
+      this.go(5);
+    }
+  }
+};
 let circles = [];
-function draw() {
-  context.globalCompositeOperation = "source-over";
-  context.fillStyle = "rgb(8,8,12)";
-  context.fillRect(0, 0, 2000, 900);
-}
-function discriminateCommand() {
-  circles.forEach(function (circle) {
-    circle.discriminateCommand();
-  });
-}
-function run() {
-  draw();
-  discriminateCommand();
-  circles.forEach(function (circle) {
-    circle.draw(context);
-  });
-}
-window.onload = function () {
+const discriminateCommand = () => circles.forEach(circle => circle.discriminateCommand());
+window.onload = () => {
   var canvas = document.getElementById('tutorial');
-  socket.on('receiveMessage', function (d) {
-    var data = JSON.parse(d); // 文字列→JSON
-    circles.push(new Circle(data));
-  });
-  if (canvas.getContext) {
-    context = canvas.getContext('2d');
-    setInterval(run, 33);
-  }
+  socket.on('receiveMessage', d => circles.push(new Circle(d)));
 };
