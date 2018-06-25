f21832999518660779a9b272e9ff890ff3276fd1
yuki3709 <32707335+yuki3709@users.noreply.github.com>
2018-06-26 01:46:28 +0900

    コンストラクターとプロトタイプはfunctionを使用

--- public/js_screen/index.js
+++ public/js_screen/index.js
@@ -4,7 +4,7 @@ const defaultProps = {
   direction: 45
 };
 const Bound = {};
-Bound.Field = e => {
+Bound.Field = function (e) {
   const self = this;
   this.canvas = e;
   if (!this.canvas.getContext) throw new Error("contextが見つかりません");
@@ -12,7 +12,7 @@ Bound.Field = e => {
   this.context.globalCompositeOperation = "source-over";
   setInterval(self.run, 33);
 };
-const Circle = data => {
+const Circle = function (data) {
   const props = JSON.parse(data);
   this.commandCount = 0;
   this.command = props.command;
@@ -29,25 +29,25 @@ Bound.Field.prototype = {
     height: 0
   },
   constructor: Bound.Field,
-  clear: () => {
+  clear: function () {
     const context = this.context;
     if (!context) return;
     const size = this.size || {};
     context.fillStyle = "#000";
     context.fillRect(0, 0, size.width, size.height);
   },
-  resize: parent => {
+  resize: function (parent) {
     this.size.width = this.canvas.width = parent.clientWidth;
     this.size.height = this.canvas.height = parent.clientHeight;
   },
-  run: () => {
+  run: function () {
     this.clear();
     discriminateCommand();
     circles.forEach(circle => circle.draw(context));
   }
 };
 Circle.prototype = {
-  draw: context => {
+  draw: function (context) {
     context.beginPath();
     context.fillStyle = '#3399FF';
     context.arc(this.locX, this.locY, 10, 0, Math.PI * 2.0, true);
@@ -55,13 +55,15 @@ Circle.prototype = {
     context.fillStyle = 'white';
     context.fillText(this.id, this.locX - 5, this.locY)
   },
-  roll: direction => this.direction = this.normalizeDirection(direction + this.direction),
-  go: distance => {
+  roll: function (direction) {
+    this.direction = this.normalizeDirection(direction + this.direction)
+  },
+  go: function (distance) {
     const radian = this.direction * Math.PI / 180;
     let distanceX = distance * Math.cos(radian);
     let distanceY = distance * Math.sin(radian);
-    var futureLocX = this.locX + distanceX;
-    var futureLocY = this.locY + distanceY;
+    let futureLocX = this.locX + distanceX;
+    let futureLocY = this.locY + distanceY;
     let direction = this.direction;
     if (futureLocX - 10 < 0 || futureLocX + 10 > 2000) {
       distanceX *= -1;
@@ -75,8 +77,10 @@ Circle.prototype = {
     this.locX += distanceX;
     this.locY += distanceY;
   },
-  normalizeDirection: direction => (direction + 360) % 360,
-  discriminateCommand: () => {
+  normalizeDirection: function (direction) {
+    (direction + 360) % 360
+  },
+  discriminateCommand: function () {
     var order = this.command[this.commandCount];
     this.commandCount = (this.commandCount + 1) % this.command.length;
     var { speedX, speedY } = this;
@@ -89,10 +93,13 @@ Circle.prototype = {
   }
 };
 let circles = [];
-const discriminateCommand = () => circles.forEach(circle => circle.discriminateCommand());
+function discriminateCommand() {
+  circles.forEach(function (circle) {
+    circle.discriminateCommand();
+  });
+}
 window.onload = () => {
-  var canvas = document.getElementById('tutorial');
+  let canvas = document.getElementById('tutorial');
   socket.on('receiveMessage', d => circles.push(new Circle(d)));
-  const field = new Bound.Field(canvas);
-  field();
+  const field = new Bound.Field(e);
 };
