dcc2d5d0845399338281ac0cf7fb6e1587a1e885
Sahara62 <cloud_1827_862@yahoo.co.jp>
2018-07-05 19:56:57 +0900

    hitEventの内容追加

--- public/js_client/index.js
+++ public/js_client/index.js
@@ -9,20 +9,37 @@ window.onload = () => {
   var go = document.getElementById('go');
   go.addEventListener('click',() => {
     prop.command.push({go:10});
-    document.getElementById('messageList').textContent = JSON.stringify(prop);
     console.log(prop);
   });
   var roll = document.getElementById('roll');
   roll.addEventListener('click',() => {
     prop.command.push({roll:90});
-    document.getElementById('messageList').textContent = JSON.stringify(prop);
     console.log(prop);
   });
   var name = document.getElementById('message');
   var inputname = document.getElementById('inputname');
   inputname.addEventListener('click',() => {
     prop.id = name.value;   //nameを決定させる。
-    document.getElementById('messageList').textContent = JSON.stringify(prop);
+    console.log(prop);
+  });
+  var onereturn = document.getElementById('onereturn');
+  onereturn.addEventListener('click', () => {
+    prop.command.pop();
+    console.log(prop);
+  });
+  var hitEventGo = document.getElementById('goEvent');
+  go.addEventListener('click',() => {
+    prop.hitEvent.push({go:10});
+    console.log(prop);
+  });
+  var hitEventRoll = document.getElementById('rollEvent');
+  go.addEventListener('click',() => {
+    prop.hitEvent.push({roll:90});
+    console.log(prop);
+  });
+  var hitEventOneReturn = document.getElementById('onereturnEvent');
+  go.addEventListener('click',() => {
+    prop.hitEvent.pop();
     console.log(prop);
   });
   var reset = document.getElementById('reset');
@@ -30,14 +47,15 @@ window.onload = () => {
     name.value = '';
     prop.id = "";
     prop.command = [];
-    document.getElementById('messageList').textContent = JSON.stringify(prop);
+    prop.hitEvent = [{roll:90}];
     console.log(prop);
   })
   var send = document.getElementById('send');
   send.addEventListener('click',() => {
     if(prop.command === 0) return fales;
-    document.getElementById('messageList').textContent = JSON.stringify(prop);
     console.log(prop);
     socket.emit('message', JSON.stringify(prop));
   });
+  document.getElementById('messageList').textContent = JSON.stringify(prop);
+  document.getElementById('hitEventList').textContent = prop.hitEventList;
 };
