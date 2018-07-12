e1f5b58230a65806b7ea7c31582982f76ad853b7
yuki3709 <32707335+yuki3709@users.noreply.github.com>
2018-07-12 14:35:54 +0900

    間隔を5ピクセルに変更

--- public/js_screen/index.js
+++ public/js_screen/index.js
@@ -43,10 +43,8 @@ Field.prototype = {
   },
   getColor: function (context) {
     this.imageData = context.getImageData(0, 0, this.size.width, this.size.height);
-    let height = this.size.height / 10;
-    let width = this.size.width / 10;
-    for (y = 0; y < this.size.height; y = y + height) {
-      for (x = 0; x < this.size.width; x = x + width) {
+    for (y = 0; y < this.size.height; y = y + 5) {
+      for (x = 0; x < this.size.width; x = x + 5) {
         let index = (y * this.size.width + x) * 4;
         let red = this.imageData.data[index]; // R
         let green = this.imageData.data[index + 1]; // G
