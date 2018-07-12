60c51116cb7afe2404531f5bd2f62e708f80f1d3
yuki3709 <32707335+yuki3709@users.noreply.github.com>
2018-07-12 13:59:05 +0900

    重くなっている原因を探す３

--- public/js_screen/index.js
+++ public/js_screen/index.js
@@ -43,8 +43,8 @@ Field.prototype = {
   },
   getColor: function (context) {
     this.imageData = context.getImageData(0, 0, this.size.width, this.size.height);
-    for (y = 0; y < this.size.height; y++) {
-      for (x = 0; x < this.size.width; x++) {
+    for (y = 0; y < this.size.height; y = y + 10) {
+      for (x = 0; x < this.size.width; x = x + 10) {
         let index = (y * this.size.width + x) * 4;
         let red = this.imageData.data[index]; // R
         let green = this.imageData.data[index + 1]; // G
