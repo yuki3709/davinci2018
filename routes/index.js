87813be2c6a11904db664add54423a906696d678
Sahara62 <cloud_1827_862@yahoo.co.jp>
2018-07-17 16:26:42 +0900

    ボタンの大きさと色を変更した

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'program-window' });
+  res.render('index', { title: 'prog' });
 });
 
 module.exports = router;
