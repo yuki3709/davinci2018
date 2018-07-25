151973b7f2adbfde4030392e7561ecc854e03366
yuki3709 <32707335+yuki3709@users.noreply.github.com>
2018-07-25 15:00:45 +0900

    消えた円は顔がなくなる

--- public/js_screen/index.js
+++ public/js_screen/index.js
@@ -36,7 +36,7 @@ Field.prototype = {
   imageData: [],
   circles: [],
   constructor: Field,
-  checkNumber: function () {
+  checkNumber: function (circle) {
     this.num.red = 0;
     this.num.fuchsia = 0;
     this.num.lime = 0;
@@ -58,24 +58,28 @@ Field.prototype = {
     for (i = 0; i < this.circles.length; i++) {
       if (this.num.red > 4) {
         if (this.circles[i].color === "red") {
+          circle.shadeDraw(this.circles[i]);
           this.circles.splice(i, 1);
           break;
         }
       }
       if (this.num.fuchsia > 4) {
         if (this.circles[i].color === "fuchsia") {
+          circle.shadeDraw(this.circles[i]);
           this.circles.splice(i, 1);
           break;
         }
       }
       if (this.num.lime > 4) {
         if (this.circles[i].color === "lime") {
+          circle.shadeDraw(this.circles[i]);
           this.circles.splice(i, 1);
           break;
         }
       }
       if (this.num.aqua > 4) {
         if (this.circles[i].color === "aqua") {
+          circle.shadeDraw(this.circles[i]);
           this.circles.splice(i, 1);
           break;
         }
