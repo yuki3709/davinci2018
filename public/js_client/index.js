d123bdc0ce87ecfc28e70cce85db07a89bc8b4cf
Sahara62 <cloud_1827_862@yahoo.co.jp>
2018-07-26 17:18:44 +0900

    リファクタリング

--- public/js_client/index.js
+++ public/js_client/index.js
@@ -1,9 +1,10 @@
-var prop = {
-  "id":"",
-  "command":[],
-  "hitEvent":[{roll:90}],
-  "color":""
+let prop = {
+  id:"",
+  command:[],
+  hitEvent:[{roll:90}],
+  color:""
 };
+let tmpCommand = prop.command;
 function getButton(discriminate){
   if("go" in discriminate){
     return "前進";
@@ -28,17 +29,19 @@ function getButton(discriminate){
       }
     })(discriminate.roll);
   }
+  // if("for" in discriminate){
+  //   setInterval(getButton(discriminate.for),30);
+  // }
 }
 function addElement(){
   console.log(prop);
-  // block.innerHTML = "";
 }
 function addCommand(){
   addElement();
-  var commandList = document.getElementById('messageList');
+  let commandList = document.getElementById('messageList');
   commandList.innerHTML = "";
-  for(var i =0; i<prop.command.length; i++){
-    var command = document.createElement("div");
+  for(let i =0; i<prop.command.length; i++){
+    let command = document.createElement("div");
     command.className = "block1";
     command.style.marginBottom = "20px";
     command.innerHTML = getButton(prop.command[i]);
@@ -47,10 +50,10 @@ function addCommand(){
 }
 function addEvent(){
   addElement();
-  var hitEventList = document.getElementById('hitEventList');
+  let hitEventList = document.getElementById('hitEventList');
   hitEventList.innerHTML = "";
-  for(var i =0; i<prop.hitEvent.length; i++){
-    var hitEvent = document.createElement("div");
+  for(let i =0; i<prop.hitEvent.length; i++){
+    let hitEvent = document.createElement("div");
     hitEvent.className = "block2";
     hitEvent.style.marginBottom = "20px";
     hitEvent.innerHTML = getButton(prop.hitEvent[i]);
@@ -58,116 +61,58 @@ function addEvent(){
   }
 }
 window.onload = () => {
-  var url = location.href;
+  let url = location.href;
   console.log(url + 'screen');
   console.log(url + '?color=red');
   console.log(url + '?color=aqua');
   console.log(url + '?color=fuchsia');
   console.log(url + '?color=lime');
+  let name = document.getElementById('userID');
   prop.color = decodeURIComponent(location.search.match(/color=(.*?)(&|$)/)[1]);
-  var go = document.getElementById('go');
-  go.addEventListener('click',() => {
-    prop.command.push({go:15});
+  const setEvent = (id, callback) => document.getElementById(id).addEventListener('click', callback);
+  const generateCallback = command => () => {
+    tmpCommand.push(command);
+    addCommand();
+  };
+  const generateRollCallback = roll => generateCallback({roll:roll});
+  setEvent('go', generateCallback({go:15}));
+  setEvent('left15', generateRollCallback(-15));
+  setEvent('left90', generateRollCallback(-90));
+  setEvent('left135', generateRollCallback(-135));
+  setEvent('right15', generateRollCallback(15));
+  setEvent('right90', generateRollCallback(90));
+  setEvent('right135', generateRollCallback(135));
+  setEvent('back', generateRollCallback(180));
+  setEvent('onereturn', () => {
+    tmpCommand.pop();
     addCommand();
   });
-  var left15 = document.getElementById('left15');
-  left15.addEventListener('click',() => {
-    prop.command.push({roll:-15});
-    addCommand();
-  });
-  var left90 = document.getElementById('left90');
-  left90.addEventListener('click',() => {
-    prop.command.push({roll:-90});
-    addCommand();
-  });
-  var left135 = document.getElementById('left135');
-  left135.addEventListener('click',() => {
-    prop.command.push({roll:-135});
-    addCommand();
-  });
-  var right15 = document.getElementById('right15');
-  right15.addEventListener('click',() => {
-    prop.command.push({roll:15});
-    addCommand();
-  });
-  var right90 = document.getElementById('right90');
-  right90.addEventListener('click',() => {
-    prop.command.push({roll:90});
-    addCommand();
-  });
-  var right135 = document.getElementById('right135');
-  right135.addEventListener('click',() => {
-    prop.command.push({roll:135});
-    addCommand();
-  });
-  var back = document.getElementById('back');
-  back.addEventListener('click',() => {
-    prop.command.push({roll:180});
-    addCommand();
-  });
-  // var name = document.getElementById('message');
-  var name = document.getElementById('userID');
-  var onereturn = document.getElementById('onereturn');
-  onereturn.addEventListener('click', () => {
-    prop.command.pop();
-    addCommand();
-  });
-  var hitEventGo = document.getElementById('goEvent');
-  hitEventGo.addEventListener('click',() => {
-    prop.hitEvent.push({go:15});
+  const generateEventCallback = hitEvent => () => {
+    prop.hitEvent.push(hitEvent);
     addEvent();
-  });
-  var hitEventLeft15 = document.getElementById('left15Event');
-  hitEventLeft15.addEventListener('click',() => {
-    prop.hitEvent.push({roll:-15});
-    addEvent();
-  });
-  var hitEventLeft90 = document.getElementById('left90Event');
-  hitEventLeft90.addEventListener('click',() => {
-    prop.hitEvent.push({roll:-90});
-    addEvent();
-  });
-  var hitEventLeft135 = document.getElementById('left135Event');
-  hitEventLeft135.addEventListener('click',() => {
-    prop.hitEvent.push({roll:-135});
-    addEvent();
-  });
-  var hitEventRight15 = document.getElementById('right15Event');
-  hitEventRight15.addEventListener('click',() => {
-    prop.hitEvent.push({roll:15});
-    addEvent();
-  });
-  var hitEventRight90 = document.getElementById('right90Event');
-  hitEventRight90.addEventListener('click',() => {
-    prop.hitEvent.push({roll:90});
-    addEvent();
-  });
-  var hitEventRight135 = document.getElementById('right135Event');
-  hitEventRight135.addEventListener('click',() => {
-    prop.hitEvent.push({roll:135});
-    addEvent();
-  });
-  var hitEventBack = document.getElementById('backEvent');
-  hitEventBack.addEventListener('click',() => {
-    prop.hitEvent.push({roll:180});
-    addEvent();
-  });
-  var hitEventOneReturn = document.getElementById('onereturnEvent');
-  hitEventOneReturn.addEventListener('click',() => {
+  }
+  const generateEventRollCallback = deg => generateEventCallback({roll:deg});
+  setEvent('goEvent', generateEventCallback({go:15}));
+  setEvent('left15Event', generateEventRollCallback(-15));
+  setEvent('left90Event', generateEventRollCallback(-90));
+  setEvent('left135Event', generateEventRollCallback(-135));
+  setEvent('right15Event', generateEventRollCallback(15));
+  setEvent('right90Event', generateEventRollCallback(90));
+  setEvent('right135Event', generateEventRollCallback(135));
+  setEvent('backEvent', generateEventRollCallback(180));
+  setEvent('onereturnEvent',() => {
     prop.hitEvent.pop();
     addEvent();
   });
-  var reset = document.getElementById('reset');
-  reset.addEventListener('click',() => {
+  setEvent('reset',() => {
     name.value = '';
     prop.id = "";
-    prop.command = [];
+    prop.command.length = 0;
     prop.hitEvent = [{roll:90}];
     addCommand();
     addEvent();
-  })
-  var send = document.getElementById('send');
-  send.addEventListener('click',() => {
+  });
+  setEvent('send',() => {
     prop.id = name.value;
     if(prop.command.length === 0 || prop.id === "" || prop.hitEvent.length === 0) {
       alert('入力されていない部分があります');
@@ -176,4 +121,12 @@ window.onload = () => {
     console.log(prop);
     socket.emit('message', JSON.stringify(prop));
   });
+  // setEvent('begin',() => {
+  //   tmpCommand = [];
+  //   prop.command.push({for:tmpCommand});
+  //   console.log(prop.command);
+  // });
+  // document.getElementById('end').addEventListener('click',() => {
+  //   tmpCommand = prop.command;
+  // });
 };
