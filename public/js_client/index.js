6d7952ba8a568a0717091b79beaffde344b14f0b
akihiro0710 <acedia2074@gmail.com>
2018-07-30 20:08:12 +0900

    デモ用の出力部分を実装

--- public/js_client/index.js
+++ public/js_client/index.js
@@ -1,15 +1,16 @@
 let prop = {
-  id:"",
-  command:[],
-  hitEvent:[{roll:90}],
-  color:""
+  id: "",
+  command: [],
+  hitEvent: [{roll: 90}],
+  color: ""
 };
 let tmpCommand = prop.command;
-function getButton(discriminate){
-  if("go" in discriminate){
+
+function getButton(discriminate) {
+  if ("go" in discriminate) {
     return "前進";
   }
-  if("roll" in discriminate){
+  if ("roll" in discriminate) {
     return (deg => {
       switch (deg) {
         case -15:
@@ -30,14 +31,16 @@ function getButton(discriminate){
     })(discriminate.roll);
   }
 }
-function addElement(){
+
+function addElement() {
   console.log(prop);
 }
-function addCommand(){
+
+function addCommand() {
   addElement();
   let commandList = document.getElementById('messageList');
   commandList.innerHTML = "";
-  for(let i =0; i<prop.command.length; i++){
+  for (let i = 0; i < prop.command.length; i++) {
     let command = document.createElement("div");
     command.className = "block1";
     command.style.marginBottom = "20px";
@@ -46,11 +49,12 @@ function addCommand(){
     commandList.scrollTop = commandList.scrollHeight;
   }
 }
-function addEvent(){
+
+function addEvent() {
   addElement();
   let hitEventList = document.getElementById('hitEventList');
   hitEventList.innerHTML = "";
-  for(let i =0; i<prop.hitEvent.length; i++){
+  for (let i = 0; i < prop.hitEvent.length; i++) {
     let hitEvent = document.createElement("div");
     hitEvent.className = "block2";
     hitEvent.style.marginBottom = "20px";
@@ -59,6 +63,7 @@ function addEvent(){
     hitEventList.scrollTop = hitEventList.scrollHeight;
   }
 }
+
 window.onload = () => {
   let url = location.href;
   console.log(url + 'screen');
@@ -73,8 +78,8 @@ window.onload = () => {
     tmpCommand.push(command);
     addCommand();
   };
-  const generateRollCallback = roll => generateCallback({roll:roll});
-  setEvent('go', generateCallback({go:15}));
+  const generateRollCallback = roll => generateCallback({roll: roll});
+  setEvent('go', generateCallback({go: 15}));
   setEvent('left15', generateRollCallback(-15));
   setEvent('left90', generateRollCallback(-90));
   setEvent('left135', generateRollCallback(-135));
@@ -90,8 +95,8 @@ window.onload = () => {
     prop.hitEvent.push(hitEvent);
     addEvent();
   }
-  const generateEventRollCallback = deg => generateEventCallback({roll:deg});
-  setEvent('goEvent', generateEventCallback({go:15}));
+  const generateEventRollCallback = deg => generateEventCallback({roll: deg});
+  setEvent('goEvent', generateEventCallback({go: 15}));
   setEvent('left15Event', generateEventRollCallback(-15));
   setEvent('left90Event', generateEventRollCallback(-90));
   setEvent('left135Event', generateEventRollCallback(-135));
@@ -99,25 +104,33 @@ window.onload = () => {
   setEvent('right90Event', generateEventRollCallback(90));
   setEvent('right135Event', generateEventRollCallback(135));
   setEvent('backEvent', generateEventRollCallback(180));
-  setEvent('onereturnEvent',() => {
+  setEvent('onereturnEvent', () => {
     prop.hitEvent.pop();
     addEvent();
   });
-  setEvent('reset',() => {
+  setEvent('reset', () => {
     name.value = '';
     prop.id = "";
     prop.command.length = 0;
-    prop.hitEvent = [{roll:90}];
+    prop.hitEvent = [{roll: 90}];
     addCommand();
     addEvent();
   });
-  setEvent('send',() => {
+  const id = (() => {
+    const idMatches = location.search.match(/id=(.*?)(&|$)/);
+    if (idMatches) {
+      return decodeURIComponent(idMatches[1]);
+    }
+  })();
+  const send = id => () => {
     prop.id = name.value;
-    if(prop.command.length === 0 || prop.id === "" || prop.hitEvent.length === 0) {
+    if (prop.command.length === 0 || prop.id === "" || prop.hitEvent.length === 0) {
       alert('入力されていない部分があります');
       return false;
     }
     console.log(prop);
-    socket.emit('message', JSON.stringify(prop));
-  });
+    socket.emit(id, JSON.stringify(prop));
+  };
+  setEvent('send', send('message'));
+  setEvent('subsend', send('demo' + id));
 };
