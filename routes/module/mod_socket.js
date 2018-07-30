c7df3fe9691ff42cf19640e8a2a852398d18ef8c
akihiro0710 <acedia2074@gmail.com>
2018-07-30 19:31:34 +0900

    demo用のsocketを追加

--- routes/module/mod_socket.js
+++ routes/module/mod_socket.js
@@ -4,9 +4,11 @@ function socket(srv) {
   var io = socket_io.listen(srv);
   io.sockets.on('connection', function (socket) {
     console.log('connected');
-    socket.on('message', function (d) {
-      io.emit('receiveMessage', d);
-    });
+    socket.on('message', d => io.emit('receiveMessage', d));
+    for (let i = 0; i < 16; i++) {
+      socket.on('demo' + i, d => io.emit('receive' + i, d))
+    }
   });
 }
+
 module.exports = socket;
