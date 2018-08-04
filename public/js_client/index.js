c1a166f008ea793d47ddff34c6af14df1d77bec8
akihiro0710 <acedia2074@gmail.com>
2018-08-04 14:33:19 +0900

    Revert "socket通信確立時にiframeを初期化"
    This reverts commit f96530b78fdee276a30427bd50045c33db8066e5.

--- public/js_client/index.js
+++ public/js_client/index.js
@@ -67,9 +67,10 @@ function addEvent() {
 window.onload = () => {
   let url = location.href;
   console.log(url + 'screen');
-  ["red", "aqua", "fuchsia", "lime"].forEach((color, i) => {
-    console.log(url + `?color=${color}&id=${i + 1}`);
-  });
+  console.log(url + '?color=red&?id=1');
+  console.log(url + '?color=aqua&?id=2');
+  console.log(url + '?color=fuchsia&?id=3');
+  console.log(url + '?color=lime&?id=4');
   document.onclick = () => {
     if (!url.match("color")) alert("色を指定してください");
   }
@@ -142,9 +143,9 @@ window.onload = () => {
     setTimeout(() => canAdd = true, 5000);
     send('message');
   });
-  socket.on('connected', ()=>{
+  setTimeout(() => {
     canvas = document.getElementById('iframe');
     canvas.src = url.replace(/\?.+/g, "screen/?id=" + socket.id);
-  });
+  }, 1000);
   setEvent('subsend', () => send('demo' + socket.id));
 };
--- routes/module/mod_socket.js
+++ routes/module/mod_socket.js
@@ -3,7 +3,6 @@ var socket_io = require('socket.io');
 function socket(srv) {
   var io = socket_io.listen(srv);
   io.sockets.on('connection', function (socket) {
-    io.sockets.socket(socket.id).emit('connected', socket.id);
     console.log('connected');
     socket.on('message', d => io.emit('receiveMessage', d));
     const id = socket.id;
